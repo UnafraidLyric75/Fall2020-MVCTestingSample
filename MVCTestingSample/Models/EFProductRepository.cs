b6abe1e71787354d972faf8fcda307037b39cd05
John Uthus <john.a.uthus.jr1@gmail.com>
2020-11-03 01:38:32 -0800

    Created ProductRepository and defualt implementation

---
+++ MVCTestingSample/Models/EFProductRepository.cs
@@ -0,0 +1,38 @@
+﻿using MVCTestingSample.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCTestingSample.Models
+{
+    public class EFProductRepository : IProductRepository
+    {
+
+
+        public Task AddProductAsync(Product p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteProductAsync(Product p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetAllProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProductIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProductAsync(Product p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MVCTestingSample/Models/Interfaces/IProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCTestingSample.Models.Interfaces
+{
+    interface IProductRepository
+    {
+        Task<Product> GetProductIdAsync(int id);
+
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+
+        Task AddProductAsync(Product p);
+
+        Task UpdateProductAsync(Product p);
+
+        Task DeleteProductAsync(Product p);
+
+    }
+}
