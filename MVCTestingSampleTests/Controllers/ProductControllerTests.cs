9adf5e553d858582bbbab932c9a7b38fef563b58
John Uthus <john.a.uthus.jr1@gmail.com>
2020-11-04 15:56:48 -0800

    Updated UnitTest project NuGet packages

---
+++ MVCTestingSampleTests/Controllers/ProductControllerTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MVCTestingSample.Controllers;
+using MVCTestingSample.Models;
+using MVCTestingSample.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCTestingSample.Controllers.Tests
+{
+    [TestClass()]
+    public class ProductControllerTests
+    {
+        [TestMethod()]
+        public async Task Index_ReturnsAViewResult_WithAListOfAllProducts()
+        {
+            // Arrange
+            Mock<IProductRepository> mockRepo = new Mock<IProductRepository>();
+            mockRepo.Setup(repo => repo.GetAllProductsAsync()).ReturnsAsync(GetProducts());
+
+            ProductController prodController = new ProductController(mockRepo.Object);
+
+            // Act
+            var result = await prodController.Index();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        private List<Product> GetProducts()
+        {
+           return new List<Product>()
+            {
+                new Product()
+                {
+                    ProductId = 1, Name = "Computer", Price = "199.99"
+                },
+                new Product()
+                {
+                    ProductId = 2, Name = "Webcam", Price = "49.99"
+                },
+                new Product()
+                {
+                    ProductId = 3, Name = "Desk", Price = "200"
+                }
+            };
+        }
+    }
+}
